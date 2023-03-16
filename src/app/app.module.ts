258bf6a1286fddced9e53a78fc8ada9f616926a5
Sahilkhan1996 <sahilkhan199655@gmail.com>
2023-03-16 20:48:59 +0530

    Initiated Data Binding project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { DataBindingComponent } from './data-binding/data-binding.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DataBindingComponent
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [DataBindingComponent]
 })
 export class AppModule { }
